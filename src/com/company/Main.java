eebd4e0bb325f11d42d6e12d4b0bb5c4f01e43e7
junwen <1317786610@qq.com>
2019-12-27 01:33:44 +0800

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Website blog = WebsiteFactory.getWebsite(WebsiteType.BLOG);
-        System.out.println(blog.getPages());
+
+        System.out.println("tt");
     }
 }
